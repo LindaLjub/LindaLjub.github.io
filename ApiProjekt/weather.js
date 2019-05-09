245c19420b4ecb0c7e62c3477239777fce85e688
Linda Lindgren <Linda.Lindgr3n@hotmail.com>
2019-05-09 19:50:56 +0200

    testdida

--- ApiProjekt/weather.js
+++ ApiProjekt/weather.js
@@ -1,26 +1,22 @@
- 
     //cors gör att man går genom en annan host för att komma vidare, för att när man har det på local host så är det inte tillåten för att den inte är tillräckligt säker.
-    const url2 = "https://cors-anywhere.herokuapp.com/http://worldtimeapi.org/api/timezone/Europe/Stockholm"; 
-    var text2 = "";
+    const url = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=Stockholm,se&appid=d2be18c42f6068bee99d3dc03695b978"; 
+    var text = "";
 
     // jokes nedanför är namnet på diven.
-    const info2 = document.getElementById('timeinfo');
+    const info = document.getElementById('weatherInfo');
     
     // hämtar från url
-    fetch(url2)
+    fetch(url)
     // får ett respons som vi gör till json format
     .then((resp) => resp.json())
     // skapar en namnlös funktion och en variabel.
     .then(function (data)
     {
             // let är en datatyp, en variabeltyp
-            let jokes2 = data.region
-            return jokes2.map(function (jokeobj2)
+            let jokes = data.weather
+            return jokes.map(function(jokeobj)
             {
-          
-                
-                          info2.innerHTML += `<br>${jokeobj2.week_number}`;
-    
+                info.innerHTML += `<br>${jokeobj.description}`;
+
             })    
     })
-   
